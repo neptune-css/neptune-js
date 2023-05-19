81399536cb44aba93f3d75a14d27614a36745a63
Colin Grahm <developer@colingrahm.de>
2023-05-20 01:13:41 +0200

    Add spinner class

--- neptune.js
+++ neptune.js
@@ -158,3 +158,55 @@ export class Link {
         }
     }
 }
+
+/**
+ * @class Button
+ * @description Create a new Neptune Button
+ * 
+ * @param {any} config Add your configuration
+ * 
+ * parent -> class or id of your target element, when null its document.body
+ * 
+ * style -> primary, cta, information, success, warning, error
+ * 
+ * animation -> linnear, eased
+ * 
+ * @example
+ * const mySpinner = new Spinner({
+ *  parent: "#container",
+ *  style: "primary",
+ *  animation: "eased"
+ * });
+ */
+export class Spinner {
+    constructor(config) {
+        // Create Link
+        const newSpinner = document.createElement('div');
+        const spinnerInner = document.createElement('div')
+        const spinnerHole = document.createElement('div')
+        newSpinner.classList.add('spinner');
+
+        spinnerInner.classList.add('spinner-inner');
+        spinnerHole.classList.add('spinner-hole');
+
+        newSpinner.appendChild(spinnerHole);
+        newSpinner.appendChild(spinnerInner);
+
+        // Setup Style
+        if (config.style != null) {
+            newSpinner.classList.add('spinner-' + config.style);
+        }
+
+        // Setup Animation
+        if (config.animation != null) {
+            newSpinner.classList.add('spin-' + config.animation)
+        }
+
+        // Append to parent element
+        if (config.parent != null) {
+            document.querySelector(config.parent).appendChild(newSpinner);
+        } else {
+            document.body.appendChild(newSpinner);
+        }
+    }
+}
