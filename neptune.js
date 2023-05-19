44bbb3d22301bf88a28505e85ac77fcee23a48ab
Colin Grahm <developer@colingrahm.de>
2023-05-20 00:31:49 +0200

    Add badge class

---
+++ neptune.js
@@ -0,0 +1,48 @@
+/**
+ * @class Badge
+ * @description Create a new Neptune Badge
+ * 
+ * @param {any} config Add your configuration
+ * 
+ * parent -> class or id of your target element, when null its document.body
+ * 
+ * text -> content of your badge
+ * 
+ * size -> s, m, l
+ * 
+ * style -> primary, accent, information, success, warning, error
+ * 
+ * @example
+ * const myBadge = new Badge({
+ *  parent: "#container",
+ *  text: "My Badge",
+ *  size: "m",
+ *  style: "primary"
+ * });
+ */
+export class Badge {
+    constructor(config) {
+        // Create Badge
+        const newBadge = document.createElement('span');
+        newBadge.classList.add('badge');
+        newBadge.innerText = config.text;
+
+        // Setup Size
+        if (config.size != null) {
+            newBadge.classList.add('badge-' + config.size);
+        }
+
+        // Setup Style
+        if (config.style != null) {
+            // Setup Style
+            newBadge.classList.add('badge-' + config.style);
+        }
+
+        // Append badge to DOM
+        if (config.parent != null) {
+            document.querySelector(config.parent).appendChild(newBadge)
+        } else {
+            document.body.appendChild(newBadge);
+        }
+    }
+}
